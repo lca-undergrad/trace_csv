d3a86dc90b681ca815c527cb76be6191eb32327d
ronny <ronnymacmaster@gmail.com>
2016-09-02 14:42:56 -0500

    trace file

---
+++ trace.py
@@ -0,0 +1,15 @@
+# trace.py
+# compile a compressed csv from the lca trace data
+# 
+# Authors: Weston D. Hill & Ronald Macmaster
+# Date: 9/2/16
+
+import re
+import csv
+import gzip
+
+
+with open("trace.txt") as traceFile:
+	for line in traceFile:
+		print line.rstrip()
+
