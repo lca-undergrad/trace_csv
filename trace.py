d78b2bbb16cda81fd1e0fba51a59d364ed3b0f2c
Weston Hill <westondhill@utexas.edu>
2016-09-02 15:42:02 -0500

    Extract info from each line with regex

--- trace.py
+++ trace.py
@@ -1,6 +1,6 @@
 # trace.py
 # compile a compressed csv from the lca trace data
-# 
+#
 # Authors: Weston D. Hill & Ronald Macmaster
 # Date: 9/2/16
 
@@ -8,10 +8,15 @@ import re
 import csv
 import gzip
 
-regex = '.*cycle:(\S+)\s+pc:v:(\S+)\s+isWrite:\S+\s+type:(.+?)paddr:p:(\S+)\s+coreid:(\S+)\s+inst:(\S+)$'
+# regular expression to find the information we need in each line
+regex = '.*cycle:(\S+)\s+pc:v:(\S+)\s+isWrite:\S+\s+type:(.+?)\s+paddr:p:(\S+)\s+coreid:(\S+)\s+inst:(\S+)$'
+
+# compile the regex into an object
 matcher = re.compile(regex)
-print regex
+
+# print a header to the csv file
+print ','.join(('cycle','pc','type','paddr','coreid','inst'))
 with open("line.txt") as traceFile:
 	for line in traceFile:
 		group = matcher.match(line)
-		print group.groups()
+        print ','.join(group.groups())
